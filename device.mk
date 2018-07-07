7620ccb4f70c5252c01b8c43831091c6002a4f81
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-07-08 00:32:31 +0200

    op9: Add a daemon for alert slider.
    Change-Id: I09914f3e1c6bd6f596de7cca6b418914f1b8cc4a

--- device.mk
+++ device.mk
@@ -34,6 +34,10 @@ PRODUCT_PACKAGES += \
 # APEX
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Alert Slider
+PRODUCT_PACKAGES += \
+    tri-state-key_daemon
+
 # Attestation
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
