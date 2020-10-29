5508e3d20f21db6cd579808efbb527d676f84be9
Rajeshwar Kurapaty <rkurapat@codeaurora.org>
2020-10-29 22:00:05 +0530

    op9: Define Netflix BSP revision.
     * Required for 1080P playback, only matters if you
       have Widevine L1 but luckily it persists after
       bootloader unlock on OP9.
    
    Change-Id: I0e0244fc360ef73dbfb6c3dcfa5607baa090ba02

--- device.mk
+++ device.mk
@@ -123,6 +123,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Netflix
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.netflix.bsp_rev=Q875-32408-1
+
 # Overlays
 PRODUCT_PACKAGES += \
     FrameworksResTarget \
