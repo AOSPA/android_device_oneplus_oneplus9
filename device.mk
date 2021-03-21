2348fbc118cc5618859ae327fe4d8e2c0ad7b17e
Alexander Koskovich <zvnexus@outlook.com>
2021-03-20 19:23:42 -0700

    op9: Declare support for device ID attestation.
    Change-Id: I0365d6726d687af290b1ba439fd558c4445f5d7f

--- device.mk
+++ device.mk
@@ -32,6 +32,10 @@ PRODUCT_PACKAGES += \
 # APEX
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Attestation
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
+
 # Boot
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.1-impl-qti \
