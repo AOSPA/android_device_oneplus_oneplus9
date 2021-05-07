d6f8a6195d6796ff97871f2368a177d0016c07fd
Alexander Koskovich <zvnexus@outlook.com>
2021-05-07 05:13:42 -0700

    op9: Use the display stack from OOS.
    Change-Id: I4c90acf27bd723a2837188594b6a43f20215943b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -93,6 +93,9 @@ DEVICE_MANIFEST_FILE += \
 DEVICE_MATRIX_FILE += \
     device/qcom/common/compatibility_matrix.xml
 
+# Hacks
+BUILD_BROKEN_DUP_RULES := true
+
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_oneplus9
 
