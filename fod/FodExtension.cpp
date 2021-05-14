1cdd7306e7b57fccb387236c3813628bd4f33632
Art_Chen <Chenxy0201@qq.com>
2021-05-14 15:51:02 +0800

    op8: fod: Return touched Z-order only.
     * OnePlus places FP layer upon the dimlayer, causing
       the former one highlighted. Only return the Z-order
       with fingerprint pressed to target the layer we need.
    
    Change-Id: I4666718018aa135a7d6387d957f136f23a5a5670

--- fod/FodExtension.cpp
+++ fod/FodExtension.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 The LineageOS Project
+ * Copyright (C) 2021 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,8 +16,8 @@
 
 #include <compositionengine/FodExtension.h>
 
-uint32_t getFodZOrder(uint32_t, bool touched) {
-    return touched ? 0xfc8 : 0xbd6;
+uint32_t getFodZOrder(uint32_t z, bool touched) {
+    return touched ? 0xfc8 : z;
 }
 
 uint64_t getFodUsageBits(uint64_t usageBits, bool) {
