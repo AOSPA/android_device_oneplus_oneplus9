15762c18d8469e6b5c1eecc4182a459da056382f
Alexander Koskovich <zvnexus@outlook.com>
2021-06-06 14:44:32 -0700

    op9: Remove pointless CNE/EGL symlinks.
     * Added these when I was just trying to match the
       KONA dump 1 for 1 but stopped doing that awhile
       ago.
    
    Change-Id: I7f845ae38ded397f881c6c4ffe9501d23ef8571e

--- Android.mk
+++ Android.mk
@@ -19,31 +19,6 @@ ifneq ($(filter oneplus9,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 # Symlinks
-CNE_SYMLINKS := $(TARGET_OUT_VENDOR_APPS)/CneApp/lib/arm64/
-$(CNE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@rm -rf $@
-	@mkdir -p $(dir $@)/vendor/lib64
-	$(hide) ln -sf /vendor/lib64/libvndfwk_detect_jni.qti.so $@/libvndfwk_detect_jni.qti.so
-
-EGL_LIBRARIES := \
-	libEGL_adreno.so \
-	libGLESv2_adreno.so \
-	libq3dtools_adreno.so
-
-EGL_32_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/lib/,$(notdir $(EGL_LIBRARIES)))
-$(EGL_32_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "EGL 32 lib link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf egl/$(notdir $@) $@
-
-EGL_64_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/lib64/,$(notdir $(EGL_LIBRARIES)))
-$(EGL_64_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "EGL lib link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf egl/$(notdir $@) $@
-
 RFS_MSM_ADSP_SYMLINKS := $(TARGET_OUT_VENDOR)/rfs/msm/adsp/
 $(RFS_MSM_ADSP_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@rm -rf $@/*
@@ -105,9 +80,6 @@ $(WLAN_FIRMWARE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	$(hide) ln -sf /mnt/vendor/persist/wlan_mac.bin $@/wlan_mac.bin
 
 ALL_DEFAULT_INSTALLED_MODULES += \
-    $(CNE_SYMLINKS) \
-    $(EGL_32_SYMLINKS) \
-    $(EGL_64_SYMLINKS) \
     $(RFS_MSM_ADSP_SYMLINKS) \
     $(RFS_MSM_CDSP_SYMLINKS) \
     $(RFS_MSM_MPSS_SYMLINKS) \
