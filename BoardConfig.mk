60831bdbb0eb1cc804fdeebf09054ce4908eaf49
Łukasz Patron <priv.luk@gmail.com>
2021-07-20 11:28:48 +0700

    op9: Implement vendor.aospa.powershare@1.0 HAL
    Change-Id: I4759d9fc35c42e73ed1ec28227e8fafdae2b7a86

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -87,6 +87,9 @@ TARGET_SCREEN_DENSITY := 420
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(DEVICE_PATH)/oneplus_vendor_framework_compatibility_matrix.xml
 
+DEVICE_FRAMEWORK_MANIFEST_FILE += \
+    $(DEVICE_PATH)/framework_manifest.xml \
+
 DEVICE_MANIFEST_FILE += \
     $(DEVICE_PATH)/manifest.xml \
     $(DEVICE_PATH)/oneplus_manifest.xml
