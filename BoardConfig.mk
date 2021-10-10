be737e37e36140d567a3913313b10093de70456d
Danny Lin <danny@kdrag0n.dev>
2021-10-10 02:58:47 -0700

    op9: Switch to Vulkan UI renderer
    We tried doing this on Android 11 and later reverted it due to fd leaks
    during prolonged video calls. However, the issue has been fixed on
    Android 12, so let's enable it again for performance.
    
    Change-Id: I1cd2ca6931aad60f9c48d40aa0212b69c68edef8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -84,6 +84,9 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 # Display
 TARGET_SCREEN_DENSITY := 420
 
+# Graphics
+TARGET_USES_VULKAN := true
+
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(DEVICE_PATH)/oneplus_vendor_framework_compatibility_matrix.xml
