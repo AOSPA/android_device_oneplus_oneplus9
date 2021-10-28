8b46a0acddc1d60e05ade25aab97b0599c22e71f
Vishalcj17 <vishalcj@aospa.co>
2021-10-28 16:03:48 +0900

    Revert "op9: Remove biometrics."
    This reverts commit 47ec0dcff0c466ea867ccaac163e52d8092b48d1.
    
    Change-Id: I9758b6c20dcd2f81f38a9dce89163d8946815d1c

--- device.mk
+++ device.mk
@@ -60,6 +60,20 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     android.hardware.authsecret@1.0.vendor
 
+# Biometrics
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
+
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.3-service.oneplus9 \
+    vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor \
+    vendor.oneplus.fingerprint.extension@1.0.vendor
+
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    persist.sys.fod.pos.x=453 \
+    persist.sys.fod.pos.y=2120 \
+    persist.sys.fod.size=174
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
