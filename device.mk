f703087484b2a4f3db07f83cc9659a933775f12d
Juhyung Park <qkrwngud825@gmail.com>
2022-05-19 23:39:17 +0900

    op9: relax refresh rate timers for OnePlus 9
    Follow https://github.com/AOSPA/android_device_oneplus_sm8150-common/commit/38d39eb9a1de21676bd2651ed43f456d589b24ff for the non-Pro variant.
    
    Pro-variant will have this overridden from its device tree.
    
    Change-Id: Iac50c8d81845b38c56a58adf5ca5ae3c35ae6ca6
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- device.mk
+++ device.mk
@@ -144,8 +144,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.disable_mask_layer_hint=1 \
     vendor.display.use_layer_ext=1 \
     ro.surface_flinger.refresh_rate_switching=true \
-    ro.surface_flinger.set_idle_timer_ms=80 \
-    ro.surface_flinger.set_touch_timer_ms=200 \
+    ro.surface_flinger.set_idle_timer_ms=4000 \
+    ro.surface_flinger.set_touch_timer_ms=4000 \
     ro.surface_flinger.set_display_power_timer_ms=1000 \
     ro.surface_flinger.use_content_detection_for_refresh_rate=true
 
