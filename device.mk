9c61594ce02a327234ead4cf819f4f1b57834fb1
Alexander Koskovich <akoskovich@pm.me>
2022-06-13 12:15:46 -0700

    oneplus9: Drop amplifier
     * Feedback is handled in kernel now.
    
    Change-Id: I94ccde2425660f14146eb304317e6d0ee1f81303

--- device.mk
+++ device.mk
@@ -41,9 +41,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
 
 # Audio
-PRODUCT_PACKAGES += \
-    audio_amplifier.lahaina
-
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.vc_call_vol_steps=9 \
     ro.vendor.audio.sdk.fluencetype=fluence \
