4efe0762db61254411cdbfcd06f4c69174bd19cd
Alexander Koskovich <akoskovich@pm.me>
2022-06-13 11:47:30 -0700

    oneplus9: Update to LE2115_11_C.61
    Change-Id: I141f24cf10973ad44e86f25b4299fe7e4d4d38ce

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -86,8 +86,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(DEVICE_PATH)/oneplus_vendor_framework_compatibility_matrix.xml
 
 DEVICE_MANIFEST_FILE += \
-    $(DEVICE_PATH)/manifest.xml \
-    $(DEVICE_PATH)/oneplus_manifest.xml
+    $(DEVICE_PATH)/manifest.xml
 
 DEVICE_MATRIX_FILE += \
     device/qcom/common/compatibility_matrix.xml
