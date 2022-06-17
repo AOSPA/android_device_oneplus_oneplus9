c820795b7f598b20f3cbafae2c71ebd8d894c3a3
Alexander Koskovich <akoskovich@pm.me>
2022-06-17 07:27:51 -0700

    oneplus9: Build vendor.qti.hardware.fm@1.0 from source
    Change-Id: I8d149f47dded9f4eaba6ca0215b8a0ed09237eb2

--- device.mk
+++ device.mk
@@ -81,7 +81,8 @@ PRODUCT_PACKAGES += \
     libbluetooth_audio_session \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor
+    vendor.qti.hardware.btconfigstore@2.0.vendor \
+    vendor.qti.hardware.fm@1.0.vendor
 
 PRODUCT_VENDOR_PROPERTIES += \
     persist.sys.fflag.override.settings_bluetooth_hearing_aid=true \
