01248cd396af3dfaa6c0a56dc03e036ca79ea425
Alexander Koskovich <akoskovich@pm.me>
2022-06-17 07:06:11 -0700

    oneplus9: oneplus_vendor_framework -> oplus_vendor_framework
    Change-Id: I3d2dd7a351695efc740ef4642ec69b1dbe53bd55

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -92,7 +92,7 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    $(DEVICE_PATH)/oneplus_vendor_framework_compatibility_matrix.xml
+    $(DEVICE_PATH)/oplus_vendor_framework_compatibility_matrix.xml
 
 DEVICE_MANIFEST_FILE += \
     $(DEVICE_PATH)/manifest.xml
