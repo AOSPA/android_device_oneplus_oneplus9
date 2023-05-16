9735862d7451c193111ad7c7f5c774c9706a850e
Vishalcj17 <vishalcj@aospa.co>
2023-05-16 17:07:22 +0900

    oneplus9: Use OSS display stack and composer.
    Change-Id: I2316d814f846180d79f94fea7fc777399905cc99

--- fingerprint/UdfpsExtension.cpp
+++ fingerprint/UdfpsExtension.cpp
@@ -5,10 +5,12 @@
 
 #include <compositionengine/UdfpsExtension.h>
 
+#include <display/drm/sde_drm.h>
+
 uint32_t getUdfpsZOrder(uint32_t z, bool touched) {
-    return touched ? 0xfc8 : z;
+    return touched ? z | FOD_PRESSED_LAYER_ZORDER : z;
 }
 
-uint64_t getUdfpsUsageBits(uint64_t usageBits, bool) {
+uint64_t getUdfpsUsageBits(uint64_t usageBits, bool /*touched*/) {
     return usageBits;
 }
