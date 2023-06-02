43d2c8d0d43ca9d24a520ae89efd65641a9a539b
Vishalcj17 <vishalcj@aospa.co>
2023-06-02 21:26:44 +0900

    oneplus9: Add support for richtap
    Change-Id: I4204fad056331373929740893a76168dc23367e6

--- device.mk
+++ device.mk
@@ -340,8 +340,12 @@ PRODUCT_PACKAGES += \
 $(call inherit-product, vendor/oneplus/oneplus9/oneplus9-vendor.mk)
 
 # Vibrator
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service.oneplus9
+PRODUCT_COPY_FILES += \
+    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v30/arm64/arch-arm-armv8-a/shared/vndk-core/android.hardware.vibrator-V1-ndk_platform.so:$(TARGET_COPY_OUT_VENDOR)/lib/android.hardware.vibrator-V1-ndk_platform.so \
+    prebuilts/vndk/v30/arm64/arch-arm64-armv8-a/shared/vndk-core/android.hardware.vibrator-V1-ndk_platform.so:$(TARGET_COPY_OUT_VENDOR)/lib64/android.hardware.vibrator-V1-ndk_platform.so
 
 # VNDK
 PRODUCT_EXTRA_VNDK_VERSIONS := 30
