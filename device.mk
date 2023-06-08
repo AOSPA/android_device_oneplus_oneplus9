dd96e3cc70691ce5751fdeeb6aceb8f51cf0a413
Juhyung Park <qkrwngud825@gmail.com>
2023-06-08 17:19:28 +0900

    oneplus9: copy audio_policy_configuration.xml from HAL
    To allow future modifications.
    
    Note on how audio configuration file paths work:
    
    This is done by audio_get_configuration_paths() function from
    system/media/audio/include/system/audio_config.h.
    
    if "ro.boot.product.vendor.sku" property is undefined:
      1. /odm/etc
      2. /vendor/etc/audio
      3. /vendor/etc
      4. /system/etc
    else
      1. /odm/etc
      if "ro.vendor.qti.va_aosp.support" == "1":
        2. /vendor/etc/audio/sku_$(ro.boot.product.vendor.sku)_qssi
      else
        2. /vendor/etc/audio/sku_$(ro.boot.product.vendor.sku)
      endif
      3. /vendor/etc/audio
      4. /vendor/etc
      5. /system/etc
    endif
    
    Note that audio_get_configuration_paths() is inlined, which means
    prebuilt blobs have its own copy and this makes things more
    complicated.
    
    For example, OnePlus 9's prebuilt sound_trigger.primary.lahaina.so
    doesn't look up /odm/etc and ro.vendor.qti.va_aosp.support and uses
    /vendor/etc/audio/sku_$(ro.boot.product.vendor.sku) first.
    
    sound_trigger.primary.lahaina.so is only interested in
    sound_trigger_platform_info.xml, which is already stored in
    /vendor/etc/audio/sku_$(ro.boot.product.vendor.sku).
    
    Change-Id: If68e6874ea867e8c6b436e23de35b571125d7e11
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- device.mk
+++ device.mk
@@ -41,6 +41,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
 
 # Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina_qssi/audio_policy_configuration.xml
+
 PRODUCT_PACKAGES += \
     audio_amplifier.lahaina
 
