630a7eea6a33cdcb11ed82ccfe23be4864edcafe
Vishalcj17 <vishalcj@aospa.co>
2023-09-02 18:33:53 +0900

    Revert "oneplus9: Bring in our old thermal config from sapphire."
    This reverts commit 8a27d5113e99ef03d92a6d992212ed45d3a45d61.
    
    Change-Id: I3554667ec0dcf27f1c0ca5fd12d612a342e1a133

--- device.mk
+++ device.mk
@@ -305,9 +305,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.systemhelper@1.0.vendor
 
 # Thermal
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/thermal/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
-
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
 
