09527a99ece21fcab1da8013d4e6b6ccb2da59b3
Vishalcj17 <vishalcj@aospa.co>
2023-12-13 01:53:28 +0900

    oneplus9: Move touch polling modes to new init file.
    Change-Id: I09cf0925f2da080ed643603cbf1f7549dfe4cb04

--- device.mk
+++ device.mk
@@ -208,6 +208,7 @@ PRODUCT_PACKAGES += \
     init.oplus.perf.rc \
     init.oplus.sensors.rc \
     init.oplus.telephony.rc \
+    init.oplus.touch.rc \
     init.oplus.wlan.rc \
     init.qti.ufs.rc \
     init.target.rc \
