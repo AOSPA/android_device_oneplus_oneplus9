b12b024034af37fdcf3be3b26623feb68867afcc
Vishalcj17 <vishalcj@aospa.co>
2023-12-25 00:35:26 +0900

    oneplus9: init: Use ODM to set device-specific properties.
    https: //gerrit.aospa.co/c/AOSPA/android_device_qcom_common/+/33047
    
    Change-Id: I850120fe6c12fd09156f94d10783f95244fbf8d7

--- init/init_oplus.cpp
+++ init/init_oplus.cpp
@@ -33,18 +33,18 @@ void OverrideProperty(const char* name, const char* value) {
  * after the original property has been set.
  */
 void vendor_load_properties() {
-    auto device = GetProperty("ro.product.product.device", "");
+    auto device = GetProperty("ro.product.odm.device", "");
     auto prjname = std::stoi(GetProperty("ro.boot.prj_version", "0"));
     auto rf_version = std::stoi(GetProperty("ro.boot.rf_version", "0"));
 
     switch (prjname) {
         case 11:
-            OverrideProperty("ro.product.product.device", "OnePlus9Pro");
+            OverrideProperty("ro.product.odm.device", "OnePlus9Pro");
             OverrideProperty("ro.surface_flinger.set_idle_timer_ms", "250");
             OverrideProperty("ro.surface_flinger.set_touch_timer_ms", "300");
             break;
         case 12:
-            OverrideProperty("ro.product.product.device", "OnePlus9");
+            OverrideProperty("ro.product.odm.device", "OnePlus9");
         default:
             LOG(ERROR) << "Unexpected prj version: " << prjname;
     }
@@ -52,37 +52,37 @@ void vendor_load_properties() {
     switch (rf_version) {
         case 11: // CN
             if (device == "OnePlus9") {
-                OverrideProperty("ro.product.product.model", "LE2110");
+                OverrideProperty("ro.product.odm.model", "LE2110");
             } else if (device == "OnePlus9Pro") {
-                OverrideProperty("ro.product.product.model", "LE2120");
+                OverrideProperty("ro.product.odm.model", "LE2120");
             }
             break;
         case 12: // TMO
             if (device == "OnePlus9") {
-                OverrideProperty("ro.product.product.model", "LE2117");
+                OverrideProperty("ro.product.odm.model", "LE2117");
             } else if (device == "OnePlus9Pro") {
-                OverrideProperty("ro.product.product.model", "LE2127");
+                OverrideProperty("ro.product.odm.model", "LE2127");
             }
             break;
         case 13: // IN
             if (device == "OnePlus9") {
-                OverrideProperty("ro.product.product.model", "LE2111");
+                OverrideProperty("ro.product.odm.model", "LE2111");
             } else if (device == "OnePlus9Pro") {
-                OverrideProperty("ro.product.product.model", "LE2121");
+                OverrideProperty("ro.product.odm.model", "LE2121");
             }
             break;
         case 21: // EU
             if (device == "OnePlus9") {
-                OverrideProperty("ro.product.product.model", "LE2113");
+                OverrideProperty("ro.product.odm.model", "LE2113");
             } else if (device == "OnePlus9Pro") {
-                OverrideProperty("ro.product.product.model", "LE2123");
+                OverrideProperty("ro.product.odm.model", "LE2123");
             }
             break;
         case 22: // NA
             if (device == "OnePlus9") {
-                OverrideProperty("ro.product.product.model", "LE2115");
+                OverrideProperty("ro.product.odm.model", "LE2115");
             } else if (device == "OnePlus9Pro") {
-                OverrideProperty("ro.product.product.model", "LE2125");
+                OverrideProperty("ro.product.odm.model", "LE2125");
             }
             break;
         default:
