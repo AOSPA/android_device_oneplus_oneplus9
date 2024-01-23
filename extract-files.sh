3fa71dd86092f74156360547eb9bc21c998d97e0
sergoops <sergoopsmoskal@gmail.com>
2024-01-24 02:11:21 +0300

    oneplus9: Patch video_system_specs.json to avoid dependency on oplus vpp filter
    Change-Id: I9e8f3937c425e568c79f9dfbe2a4a8046d00622b

--- extract-files.sh
+++ extract-files.sh
@@ -66,6 +66,9 @@ function blob_fixup() {
             ;;
         vendor/etc/media_lahaina/video_system_specs.json)
             sed -i "/max_retry_alloc_output_timeout/ s/1000/0/" "${2}"
+            sed -i "/pipelining/ s/^/\/\//" "${2}"
+            sed -i "/liboplusvppfilter.so/ s/^/\/\//" "${2}"
+            sed -i "/libqc2vppfilter.so/ s/,$//" "${2}"
             ;;
         vendor/lib/libgui1_vendor.so)
             "${PATCHELF}" --replace-needed "libui.so" "libui-v30.so" "${2}"
