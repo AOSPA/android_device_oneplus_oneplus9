963c9b583d24ef2460479ebffffb268de38377d0
Vishalcj17 <vishalcj@aospa.co>
2024-01-25 01:25:59 +0900

    oneplus9: kernel: Use full LLVM.
    Change-Id: I2f49800d8cdde8f70bedc46c5ff3141944643033

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -127,6 +127,7 @@ BOARD_KERNEL_BINARIES := kernel
 BOARD_KERNEL_PAGESIZE := 4096
 KERNEL_DEFCONFIG := vendor/lahaina-qgki_defconfig
 USE_KERNEL_AOSP_LLVM := true
+KERNEL_FULL_LLVM := true
 
 # Lineage Health
 TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH := /sys/class/oplus_chg/battery/chg_enable
