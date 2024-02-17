0e7ddf3958d8e4cef0697def3870006d774e1420
Jake Weinstein <jake@aospa.co>
2024-02-17 17:22:18 +0900

    oneplus9: Move IRQ configuration to device/qcom/common
    Change-Id: I12444a4c90527c8c01214e0a082220cd16f7ced2

--- device.mk
+++ device.mk
@@ -193,10 +193,6 @@ PRODUCT_PACKAGES += \
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# Performance
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
-
 # Platform
 TARGET_BOARD_PLATFORM := lahaina
 
