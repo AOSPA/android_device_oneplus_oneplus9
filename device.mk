6f272b4c90b427d2931cdd6af4afac8bfa4e3753
Michael Bestas <mkbestas@lineageos.org>
2024-09-26 01:39:32 +0300

    oneplus9: Switch to AIDL thermal HAL
    Change-Id: I6ae621194044ab66fad69b9ef997b3b347bf6120

--- device.mk
+++ device.mk
@@ -259,7 +259,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.qti
+    android.hardware.thermal-service.qti
 
 # Update Engine
 PRODUCT_PACKAGES += \
